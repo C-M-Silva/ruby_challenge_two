995f9eed49eb6d7d2e32dda587072ef6b03a3208
Cristiano Silva <cristianomiguelsilva@outlook.com>
2022-12-01 14:08:45 +0000

    Completed challege2

---
+++ program/lib/password_manager.rb
@@ -0,0 +1,32 @@
+class PasswordManager
+  # First we initalise the password has as a global variable using the @ 
+  def initialize
+      @passwords = {}
+  end
+  
+  # This the next function (add) takes two arguments (service, password), 
+  # which in turn provide the ability to store that information into the 
+  # @password hash created within the initialize() function.
+  # Importantly, some simple logic has been impelled which checks if the password length 
+  # is >= 8 and if there is at least one special character. 
+
+  def add(service, password)
+      arr = ['!', '@', '$', '%', '&']
+      if (password.count password).to_i >= 8 && arr.any? {|s_char| password.include? s_char} == true
+          return @passwords[service] = password
+      else 
+          return
+      end
+  end
+  
+  # Password_for() takes one argument and by presenting it with the ‘key’ information (service)  is returns the values (password)
+  def password_for(service)
+    return @passwords[service]
+  end
+  
+  # services() returns all the keys available in the hash using the .keys() method.
+  def services
+    return @passwords.keys
+  end
+  
+end
